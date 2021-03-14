c149a70ffc974252d6266940620b2eceb82e6e15
Darryl Lardizabal <dsteven12@gmail.com>
2021-03-14 13:07:59 -0700

    Feature: Implemented market.html. Beginning work on sending data to templates.

--- market.py
+++ market.py
@@ -2,9 +2,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/home')
 def home_page():
     return render_template('home.html')
 
-@app.route('/about/<username>')
-def about_page(username):
-    return f'<h1>About Page of {username}</h1>'
+@app.route('/market')
+def market_page():
+    return render_template('market.html', item_name='Phone')
