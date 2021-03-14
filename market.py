861b1c7cc502ebc22f71d1d941bfc8e6015b1bac
Darryl Lardizabal <dsteven12@gmail.com>
2021-03-14 12:44:52 -0700

    Feature: Changed return from strings to using render_template convention with a home.html template.

--- market.py
+++ market.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return render_template('home.html')
+
+@app.route('/about/<username>')
+def about_page(username):
+    return f'<h1>About Page of {username}</h1>'
